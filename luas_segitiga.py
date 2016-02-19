3c4f7a89d377953711f34d3e2f4d62e9f66ef924
almanfawzy <almanfawzy.a2@gmail.com>
2016-02-19 15:05:17 +0700

    Create luas_segitiga.py

---
+++ luas_segitiga.py
@@ -0,0 +1,10 @@
+print ('Menghitung luas segitiga')
+def hitung_luas_segitiga(height, base):
+    return height * base / 2
+
+hasil1 = hitung_luas_segitiga(10, 2)
+hasil2 = hitung_luas_segitiga(30, 5)
+hasil3 = hitung_luas_segitiga(15, 2)
+print hasil1
+print hasil2
+print hasil3
