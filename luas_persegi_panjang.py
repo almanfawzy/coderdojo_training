378d75f344ad45b7ca3826e7037c11826bd43fa0
almanfawzy <almanfawzy.a2@gmail.com>
2016-02-19 15:04:37 +0700

    Create luas_persegi_panjang.py

---
+++ luas_persegi_panjang.py
@@ -0,0 +1,11 @@
+print ('Menghitung luas persegi panjang')
+print ('Panjang = 7, Lebar = 6')
+panjang = 7
+lebar = 6
+luas = panjang * lebar
+
+print "Luas persegi panjang adalah", luas
+
+print ('Menghitung luas segitiga')
+def hitung_luas_segitiga(height, base):
+    return height * base / 2
